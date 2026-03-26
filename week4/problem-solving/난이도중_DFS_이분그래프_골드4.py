3d010eddb043a09fd3e000790e45372f53bc98ec
JUHOSEOK <jhseok0107@gmail.com>
2026-03-26 23:48:55 +0900

    그래프DFSBFS_DFS와BFS_실버2.py, DFS_이분그래프_골드4.py

--- week4/problem-solving/난이도중_DFS_이분그래프_골드4.py
+++ week4/problem-solving/난이도중_DFS_이분그래프_골드4.py
@@ -1,2 +1,62 @@
 # DFS - 이분 그래프 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/1707
+
+# bfs로 탐색하는데 서로 연관된거 있으면은 while queue 끝날때 +1 해주기?
+
+# bfs()
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(start, vertex, visited_queue):
+
+    queue = deque([start])
+
+    visited_queue[start] = 1
+    while queue:
+
+        popleft_node = queue.popleft()
+
+
+        for neighbor in (graph[popleft_node]):
+            if visited_queue[neighbor] == 0:
+                queue.append(neighbor)
+                visited_queue[neighbor] = visited_queue[popleft_node] * -1
+
+            elif visited_queue[neighbor] == visited_queue[popleft_node]:
+                return False
+
+    return True
+
+
+case = int(input())
+
+for _ in range(case):
+
+    vertex, edge = map(int,input().split())
+    graph = {i : [] for i in range(1, vertex+1)}
+    visited_queue = [0] * (vertex+1)
+    is_ans = True
+
+    for j in range(edge):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+
+    for i in range(1, vertex+1):
+        if visited_queue[i] == 0:
+            if not bfs(i,vertex, visited_queue):
+                is_ans = False
+                break
+
+    graph = {}
+
+    if is_ans:
+        print("YES")
+    else:
+        print("NO")
+
+
+
+
+
--- week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
@@ -1,2 +1,82 @@
 # 그래프, DFS, BFS - DFS와 BFS (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1260
+from collections import deque
+
+# 아이디어: 그냥 그래프 주어지면 bfs, dbs 탐색한 결과 출력하는거, 방문할수 있는 정점이 여러개일때는 작은거부터 sort()
+
+# 함수 bfs(): bfs로 탐색하는 함수, dbf(): dbf로 탐색하는 함수
+
+# O(E log v) = 10^5 < 2*(10^8)
+
+
+# 입력:
+# 1. 정점의개수, 간선의개수, 시작할 정점의 번호
+# 2. 간선이 연결하는 두 정점의 번호
+#
+# 출력:
+# dbs로 탐색한 결과
+# bfs로 탐색한 결과
+# 4 5 1
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
+
+vertex, edge, start = map(int,input().split())
+
+graph = {i:[] for i in range(1, vertex+1)}
+for i in range(edge):
+    u, v = map(int, input().split())
+
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs():
+
+    stack = [start]
+    visited_stack = []
+    while stack:
+        pop_node = stack.pop()
+
+        if not pop_node in visited_stack:
+            visited_stack.append(pop_node)
+
+            for neighbor in sorted(graph[pop_node], reverse=True):
+                    stack.append(neighbor)
+
+    return visited_stack
+
+
+def bfs():
+
+    queue = deque([start])
+    visited_queue = [start]
+    while queue:
+
+        popleft_node = queue.popleft()
+        # print("뺸값", popleft_node)
+
+        for neighbor in (graph[popleft_node]):
+            if not neighbor in visited_queue:
+                queue.append(neighbor)
+                visited_queue.append(neighbor)
+                # print("넣은값", neighbor)
+
+    return visited_queue
+
+
+
+
+
+print(*dfs())
+print(*bfs())
+
+
+
+
+
+
+
+
+
